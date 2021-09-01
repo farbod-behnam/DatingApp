efb7c2fe128ed719b25e7991029bfe7a0aced4c7
farbod-behnam <farbod.behnam@gmail.com>
2021-09-01 18:36:42 +0430

    added seed data, implemented repository pattern, and AutoMapper

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+       public static int CalculateAge(this DateTime dateOfBirth)
+       {
+           DateTime today = DateTime.Today;
+           int age = today.Year - dateOfBirth.Year;
+
+           if (dateOfBirth.Date > today.AddYears(-age))
+            age--;
+
+           return age;
+       } 
+    }
+}
